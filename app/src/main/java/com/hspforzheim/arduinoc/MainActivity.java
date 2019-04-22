90e357aac6f796a8a19b0a533a935ac52992aeb4
ka-repo <kariem.abouelhassan@web.de>
2019-04-22 23:05:05 +0200

    fixed method onNavigationItemSelected to start new activity when menu item is clicked.

--- app/src/main/java/com/hspforzheim/arduinoc/MainActivity.java
+++ app/src/main/java/com/hspforzheim/arduinoc/MainActivity.java
@@ -1,20 +1,33 @@
 package com.hspforzheim.arduinoc;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private MenuItem item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavigationView mNavigationView = findViewById(R.id.navgigation_bar_id);
+
+        if (mNavigationView != null) {
+            mNavigationView.setNavigationItemSelectedListener(this);
+        }
 
         /**
         Ab hier wird die Menuleiste definiert.
@@ -29,11 +42,38 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    /**
+     * Wird benötigt um das Klicken des Menu-Icons zu ermöglichen.
+     *
+     * @param item
+     * @return true if toggled
+     */
     public boolean onOptionsItemSelected(MenuItem item) {
         if(mToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    /**
+     * Hier werden nun beim Klicken der Menu-Punkte neue Activities aufgerufen,
+     * also eine neue Klasse mit einer eigenen oncreate() und in der App einer eigenen Seite.
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.bluetooth:
+                Intent i = new Intent(MainActivity.this, findArduino.class);
+                startActivity(i);
+                break;
+
+            default:
+                break;
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
---
+++ app/src/main/java/com/hspforzheim/arduinoc/findArduino.java
@@ -0,0 +1,14 @@
+package com.hspforzheim.arduinoc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class findArduino extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_arduino);
+
+    }
+}
